0cc88d9af7742351b954872644653e1a8d9e429e
PatrickJoshua <bakulinglings@gmail.com>
2014-09-23 03:43:38 +0800

    Added Records History Table on Database
    Upcoming issues:
    Reset number everyday
    Get last queue number from history table within the day

--- QueueWebApp/src/java/Common.java
+++ QueueWebApp/src/java/Common.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
+import java.sql.PreparedStatement;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -39,6 +40,27 @@ public class Common {
         return con;
     }
     
+    public static void add2History(Connection con, int num, String cellNo, boolean vip, int ref, String name)
+    {
+        try
+        {
+            PreparedStatement ps = con.prepareStatement("insert into RECORDSHISTORY values (" + num + ",'" + cellNo + "'," + vip + "," + ref + ",'" + name + "',?,?)");
+            
+            java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());  //get current date
+            ps.setDate(1, sqlDate);     //set date on SQL statement
+            
+            java.sql.Time sqlTime = new java.sql.Time(new java.util.Date().getTime());  //get current time
+            ps.setTime(2, sqlTime);     //set time on SQL statement
+            
+            ps.executeUpdate();     //execute SQL statement
+            ps.close();
+        }
+        catch (SQLException sqle)
+        {
+            System.err.println(sqle.getMessage());
+        }
+    }
+    
     public static int add2DB(Connection con, String cellNo, String name, boolean vip)
     {
         int lastNumber = 0;     //holds the last number in the DB
@@ -52,28 +74,6 @@ public class Common {
             //detect duplicate
             while(rs.next())    //loop until reached the last VIP
             {
-                /*if(vip)
-                {
-                    if(cellNo.equals(rs.getString("MOBILENUM")) || name.equalsIgnoreCase(rs.getString("NAME")))      //true if duplicate
-                    {
-                        lastNumber = rs.getInt("NUM");     //set last number from db
-                        lastNumber--;       //temporarily decrement (will be incremented before return)
-                        ref = rs.getInt("REF");
-                        duplicate = true;
-                        break;
-                    }
-                }
-                else
-                {
-                    if(cellNo.equals(rs.getString("MOBILENUM")))      //true if duplicate
-                    {
-                        lastNumber = rs.getInt("NUM");     //set last number from db
-                        lastNumber--;       //temporarily decrement (will be incremented before return)
-                        ref = rs.getInt("REF");
-                        duplicate = true;
-                        break;
-                    }
-                }*/
                 if(vip)
                 {
                     if(cellNo.equals(rs.getString("MOBILENUM")) || name.equalsIgnoreCase(rs.getString("NAME")))      //true if duplicate
@@ -99,12 +99,16 @@ public class Common {
             {
                 ref = Common.generateReferenceNo();
                 if(!rs.last())      //if no one in queue is VIP
-                    stmt.execute("insert into QUEUETBL values (1,'" + cellNo + "'," + vip + "," + ref + ",'" + name + "')");  //insert value to table (1)            
+                {
+                    stmt.executeUpdate("insert into QUEUETBL values (1,'" + cellNo + "'," + vip + "," + ref + ",'" + name + "')");  //insert value to table (1)            
+                    add2History(con,1,cellNo,vip,ref,name);     //add to recordshistory table
+                }
                 else
                 {
                     lastNumber = rs.getInt("NUM");       //change this in the future to get last value from a history table
                     lastNumber++;   //use actual position, and write to database
-                    stmt.execute("insert into QUEUETBL values (" + lastNumber + ",'" + cellNo + "'," + vip + "," + ref + ",'" + name + "')");  //insert value to table
+                    stmt.executeUpdate("insert into QUEUETBL values (" + lastNumber + ",'" + cellNo + "'," + vip + "," + ref + ",'" + name + "')");  //insert value to table
+                    add2History(con,lastNumber,cellNo,vip,ref,name);    //add to reccordshistory table
                     lastNumber--;   //revert to lastNumber after writing to database
                 }
             }
